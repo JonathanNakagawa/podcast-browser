6f1fc99cc59fddaac4a2581f8c3784d847466fbc
Jonathan Nakagawa <jnakagawa17@gmail.com>
2020-05-09 13:30:22 -1000

    Reworked podcast row css

--- client/src/components/structure/PodcastRow.js
+++ client/src/components/structure/PodcastRow.js
@@ -13,6 +13,9 @@ function PodcastRow() {
                 <TitleCard imageURL="https://images.squarespace-cdn.com/content/52d66949e4b0a8cec3bcdd46/1391195775824-JVU9K0BX50LWOKG99BL5/Hello+Internet.003.png?content-type=image%2Fpng"/>
                 <TitleCard imageURL="https://images.squarespace-cdn.com/content/52d66949e4b0a8cec3bcdd46/1391195775824-JVU9K0BX50LWOKG99BL5/Hello+Internet.003.png?content-type=image%2Fpng"/>
                 <TitleCard imageURL="https://images.squarespace-cdn.com/content/52d66949e4b0a8cec3bcdd46/1391195775824-JVU9K0BX50LWOKG99BL5/Hello+Internet.003.png?content-type=image%2Fpng"/>
+                <TitleCard imageURL="https://images.squarespace-cdn.com/content/52d66949e4b0a8cec3bcdd46/1391195775824-JVU9K0BX50LWOKG99BL5/Hello+Internet.003.png?content-type=image%2Fpng"/>
+                <TitleCard imageURL="https://images.squarespace-cdn.com/content/52d66949e4b0a8cec3bcdd46/1391195775824-JVU9K0BX50LWOKG99BL5/Hello+Internet.003.png?content-type=image%2Fpng"/>
+                <TitleCard imageURL="https://images.squarespace-cdn.com/content/52d66949e4b0a8cec3bcdd46/1391195775824-JVU9K0BX50LWOKG99BL5/Hello+Internet.003.png?content-type=image%2Fpng"/>
             </ul>
         </div>
     );
