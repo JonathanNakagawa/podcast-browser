e20d388c8005f093a5986ad050231c0b1bd83d0d
Jonathan Nakagawa <jnakagawa17@gmail.com>
2020-05-09 18:31:24 -1000

    Added title card description

--- client/src/components/structure/PodcastRow.js
+++ client/src/components/structure/PodcastRow.js
@@ -7,15 +7,38 @@ import './structure.css';
 function PodcastRow() {
     return(
         <div className="podcast-row">
-            <div className="podcast-row__title">Temporary</div>
+            <div className="podcast-row__title">Suggested</div>
             <ul className="podcast-row__items">
-                <TitleCard imageURL="https://images.squarespace-cdn.com/content/52d66949e4b0a8cec3bcdd46/1391195775824-JVU9K0BX50LWOKG99BL5/Hello+Internet.003.png?content-type=image%2Fpng"/>
-                <TitleCard imageURL="https://images.squarespace-cdn.com/content/52d66949e4b0a8cec3bcdd46/1391195775824-JVU9K0BX50LWOKG99BL5/Hello+Internet.003.png?content-type=image%2Fpng"/>
-                <TitleCard imageURL="https://images.squarespace-cdn.com/content/52d66949e4b0a8cec3bcdd46/1391195775824-JVU9K0BX50LWOKG99BL5/Hello+Internet.003.png?content-type=image%2Fpng"/>
-                <TitleCard imageURL="https://images.squarespace-cdn.com/content/52d66949e4b0a8cec3bcdd46/1391195775824-JVU9K0BX50LWOKG99BL5/Hello+Internet.003.png?content-type=image%2Fpng"/>
-                <TitleCard imageURL="https://images.squarespace-cdn.com/content/52d66949e4b0a8cec3bcdd46/1391195775824-JVU9K0BX50LWOKG99BL5/Hello+Internet.003.png?content-type=image%2Fpng"/>
-                <TitleCard imageURL="https://images.squarespace-cdn.com/content/52d66949e4b0a8cec3bcdd46/1391195775824-JVU9K0BX50LWOKG99BL5/Hello+Internet.003.png?content-type=image%2Fpng"/>
-                <TitleCard imageURL="https://images.squarespace-cdn.com/content/52d66949e4b0a8cec3bcdd46/1391195775824-JVU9K0BX50LWOKG99BL5/Hello+Internet.003.png?content-type=image%2Fpng"/>
+                <TitleCard 
+                    imageURL="https://images.squarespace-cdn.com/content/52d66949e4b0a8cec3bcdd46/1391195775824-JVU9K0BX50LWOKG99BL5/Hello+Internet.003.png?content-type=image%2Fpng"
+                    title="Hello Internet"
+                    author="CGP Grey"
+                />
+                <TitleCard 
+                    imageURL="https://images.squarespace-cdn.com/content/52d66949e4b0a8cec3bcdd46/1391195775824-JVU9K0BX50LWOKG99BL5/Hello+Internet.003.png?content-type=image%2Fpng"
+                    title="Hello Internet"
+                    author="CGP Grey"
+                />
+                <TitleCard 
+                    imageURL="https://images.squarespace-cdn.com/content/52d66949e4b0a8cec3bcdd46/1391195775824-JVU9K0BX50LWOKG99BL5/Hello+Internet.003.png?content-type=image%2Fpng"
+                    title="Hello Internet"
+                    author="CGP Grey"
+                />
+                <TitleCard 
+                    imageURL="https://images.squarespace-cdn.com/content/52d66949e4b0a8cec3bcdd46/1391195775824-JVU9K0BX50LWOKG99BL5/Hello+Internet.003.png?content-type=image%2Fpng"
+                    title="Hello Internet"
+                    author="CGP Grey"
+                />
+                <TitleCard 
+                    imageURL="https://images.squarespace-cdn.com/content/52d66949e4b0a8cec3bcdd46/1391195775824-JVU9K0BX50LWOKG99BL5/Hello+Internet.003.png?content-type=image%2Fpng"
+                    title="Hello Internet"
+                    author="CGP Grey"
+                />
+                <TitleCard 
+                    imageURL="https://images.squarespace-cdn.com/content/52d66949e4b0a8cec3bcdd46/1391195775824-JVU9K0BX50LWOKG99BL5/Hello+Internet.003.png?content-type=image%2Fpng"
+                    title="Hello Internet"
+                    author="CGP Grey"
+                />
             </ul>
         </div>
     );
--- client/src/components/structure/TitleCard.js
+++ client/src/components/structure/TitleCard.js
@@ -5,8 +5,12 @@ import './structure.css';
 function TitleCard(props) {
     console.log("url(" + props.imageURL + ")")
     return(
-        <div className="title-card" style={{backgroundImage: "url(" + props.imageURL + ")"}}>
-
+        <div className="title-card">
+            <div className="title-card__image" style={{backgroundImage: "url(" + props.imageURL + ")"}}/>
+            <div className="title-card__description">
+                <div className="title-card__description__title">{props.title}</div>
+                <div className="title-card__description__author">{props.author}</div>
+            </div>
         </div>
     );
 }
